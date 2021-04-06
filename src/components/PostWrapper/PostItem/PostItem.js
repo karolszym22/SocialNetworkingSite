192c17a4a6e6e387eabcbfa6b7349ce9db909cd7
Karol Szymański <karol2134@op.pl>
2021-04-06 18:21:17 +0200

    add post's image, add new clasess to PostItem.css

--- src/components/PostWrapper/PostItem/PostItem.js
+++ src/components/PostWrapper/PostItem/PostItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './PostItem.css';
-
+import karolSzymanskiImage from '../../../images/profile.png'
 const PostItem = () => 
 (
-    <li className = "PostItemWrapper">
-        item1;
+    <li className = "postItem__wrapper">
+        <img src={karolSzymanskiImage} className="PostItem__image"/>
+        <div>
+            <h2>Karol Szymański</h2>
+            <p>Fajny ten fakebook, taki nie za bogaty</p>
+            <button>Lubię to</button>
+            <button>Skomentuj</button>
+        </div>
+      
     </li>
 )
 
