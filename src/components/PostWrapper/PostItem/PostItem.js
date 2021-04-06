eba151985fa38fe5eba49acdac7ea1e82471190e
Karol Szymański <karol2134@op.pl>
2021-04-06 20:31:01 +0200

    PostItem components style

--- src/components/PostWrapper/PostItem/PostItem.js
+++ src/components/PostWrapper/PostItem/PostItem.js
@@ -4,12 +4,14 @@ import karolSzymanskiImage from '../../../images/profile.png'
 const PostItem = () => 
 (
     <li className = "postItem__wrapper">
-        <img src={karolSzymanskiImage} className="PostItem__image"/>
+        <div className = "postItem__imageContainer">
+          <img src={karolSzymanskiImage} className="imageContainer__image"/>
+        </div>
         <div>
             <h2>Karol Szymański</h2>
-            <p>Fajny ten fakebook, taki nie za bogaty</p>
-            <button>Lubię to</button>
-            <button>Skomentuj</button>
+            <p className="postItem__description">Fajny ten fakebook, taki nie za bogaty</p>
+            <button className="postItem__button postItem__button--like">Lubię to</button>
+            <button className="postItem__button postItem__button--comment">Skomentuj</button>
         </div>
       
     </li>
