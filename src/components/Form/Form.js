fdd44f2465a21924829118f7e3fe150d243befee
Karol Szymański <karol2134@op.pl>
2021-04-07 19:15:40 +0200

    add requried do input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,9 +4,9 @@ import style from './Form.module.scss';
 const Form = ({submitFn}) => 
 (
     <form className={style.wrapper} onSubmit={submitFn}>
-        <input placeholder="name" name="name" />
-        <input placeholder="link" name="link" />
-        <input placeholder="image" name="image" />
+        <input required placeholder="name" name="name" />
+        <input required placeholder="link" name="link" />
+        <input required placeholder="image" name="image" />
         <textarea placeholder="description"
          name="desription" />
          <button className={style.submit} type="submit">
