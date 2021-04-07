befadc295feb5099ed1dc38ff08647d72c36d4ee
Karol Szymański <karol2134@op.pl>
2021-04-07 16:17:12 +0200

    export data from folder "data" and deleted that folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import PostItem from './components/PostWrapper/PostItem/PostItem';
 import PostWrapper from './components/PostWrapper/PostWrapper';
 import './index.css';
 
-
-const App = () => 
-(
-  <div>
-  
-    <PostWrapper />
-  </div>
-)
-
+class App extends React.Component
+{
+  state = {
+    items: [{
+      name: "Karol",
+      lastName: "Szymański",
+      postContent: "Jestem Karol, i robię pseudo facebooka, tak więc....no tak",
+      image: profile,
+      id: "post1"
+  },
+  {
+      name: "Radek",
+      lastName: "Kaczorowski",
+      postContent: "Jestem Radek, i robię pseudo facebooka, tak więc....no tak",
+      image: radek,
+      id: "post2"
+  },
+  {
+      name: "Rafał",
+      lastName: "Szewczyk",
+      image: rafał,
+      id: "post3"
+  },
+  {
+      name: "Dorota",
+      lastName: "Szymańska",
+      postContent: "Jestem Dorota, i robię pseudo facebooka, tak więc....no tak",
+      image: dorota,
+      id: "post4"
+  }
+]};
+  }
+  render()
+  {
+    return(
+      <div>
+        <PostItem items={items}/>
+      </div>
+    )
+  }
+}
 export default App;
