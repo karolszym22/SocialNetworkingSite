78f82e099878ace12b13d6fcc313c1fe3d48867a
Karol Szymański <karol2134@op.pl>
2021-04-09 17:34:41 +0200

    new atoms: Heading.js, Paragraph.js and new Molecule: PostCart.js

---
+++ src/components/molecules/PostCart/PostCart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import Paragraph from 'components/atoms/Paragraph';
+import Heading from 'components/atoms/Heading';
+import Button from '../../atoms/Button/reactionButtons';
+
+const StyledWrapper = styled.div`
+padding: 17px 30px;
+`
+
+const PostCard = () =>
+{
+    <StyledWrapper>
+        <Heading>
+            Karol Szymański
+        </Heading>
+        <Paragraph>
+            Dobra,wstawiłem kolejny posta, sorry za to, no ale.....musiałem. Tak więc dobranoc, słodkich snów
+        </Paragraph>
+    </StyledWrapper>
+}
