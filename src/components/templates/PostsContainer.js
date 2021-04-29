a71191352b84d8c4326d7cf8d2a159d8a30f7942
Karol Szymański <karol2134@op.pl>
2021-04-29 17:32:44 +0200

    add templates folder and put new file : PostsContainer.js

---
+++ src/components/templates/PostsContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../../theme/GlobalStyle';
+import { theme } from '../../theme/mainTheme';
+
+const PostsContainer = ({ children }) => (
+    
+    <div>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <>
+       {children}
+      </> 
+    </ThemeProvider>
+  </div>
+
+);
+
+export default PostsContainer;
