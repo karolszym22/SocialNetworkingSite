9d288427372863277d03315be132dbe0b45114e9
Karol Szymański <karol2134@op.pl>
2021-05-04 18:05:38 +0200

    update NavigationMenu.js

--- src/components/organisms/NavigationMenu.js
+++ src/components/organisms/NavigationMenu.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import HeaderButton from '../atoms/Button/headerButtons/HeaderButton'
+import friendsIcon from '../../assets/icons/header/friends.svg'
+import informationIcon from '../../assets/icons/header/information.svg'
+import messageIcon from '../../assets/icons/header/message.svg'
 
 const NavigationMenu = () =>
 (
     <div>
-        
+        <p>Fakebook</p>
+        <div>
+            <HeaderButton as={Link} to="/" icon={friendsIcon} />
+            <HeaderButton as={Link} icon={messageIcon} />
+            <HeaderButton as={Link} icon={informationIcon} />
+        </div>
     </div>
 )
