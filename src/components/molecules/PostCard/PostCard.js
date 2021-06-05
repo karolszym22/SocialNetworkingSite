5e3fa4485fbe839fd1df75c89a697e6cca07329e
Karol Szymański <karol2134@op.pl>
2021-06-05 13:16:45 +0200

    edit PostCard.js

--- src/components/molecules/PostCard/PostCard.js
+++ src/components/molecules/PostCard/PostCard.js
@@ -4,16 +4,62 @@ import PostHeading from '../../atoms/Heading/PostHeading';
 
 
 const Container = styled.div`
-padding: 17px 30px;
 border: 1px solid black;
-height: 400px;
 width: 500px;
+display: flex;
+flex-direction: column;
+margin-top: 30px;
+
 `
+
+const AuthorContainer = styled.div`
+margin: 10px 20px;
+width: 460px;
+padding: 20px;
+border: 1px solid black;
+height: 55px;
+display: flex;
+`
+
+const AuthorImage = styled.div`
+height: 100%;
+display: flex;
+align-items: center;
+margin: 0px 5px;
+
+`
+const Image = styled.div`
+border-radius: 50%;
+border: 1px solid black;
+height: 50px;
+width: 50px;
+background-image: url("profile.png");
+`
+const PostContent = styled.p`
+
+width: 460px;
+margin: 15px 20px;
+box-sizing: border-box;
+padding: 5px;
+min-height: 100px;
+border: 1px solid black;
+
+`
+
 
 
 const PostCard = () =>
 (
     <Container>
+      <AuthorContainer>
+        <AuthorImage>
+            <Image/>
+        </AuthorImage>
+        <h4>Karol Szymański</h4>
+      </AuthorContainer> 
+      <PostContent>
+          <p>asdsadasdasdasdasdasdas</p>
+      </PostContent>
         <p>Fakebook</p>
     </Container>
   
--- src/components/templates/PostsContainer.js
+++ src/components/templates/PostsContainer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import GlobalStyle from '../../theme/GlobalStyle';
 import { theme } from '../../theme/mainTheme';
 import PostCard from '../molecules/PostCard/PostCard'
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
 
 const PostsContainer = ({ children }) => (
     
@@ -13,11 +20,11 @@ const PostsContainer = ({ children }) => (
 
     
     <ThemeProvider theme={theme}>
-    <>
+    <Container>
     <PostCard/>
 
       {children}
-      </>
+      </Container>
     </ThemeProvider>
   </div>
 
