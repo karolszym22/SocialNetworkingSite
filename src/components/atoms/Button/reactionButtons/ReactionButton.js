7235758904a27a1ec439f08c9fa26bbeda5d3ef5
Karol Szymański <karol2134@op.pl>
2021-06-14 15:01:06 +0200

    ReactionButton.js: changed border from none to 1px solid black

--- src/components/atoms/Button/reactionButtons/ReactionButton.js
+++ src/components/atoms/Button/reactionButtons/ReactionButton.js
@@ -4,10 +4,10 @@ const ReactionButton = styled.button` //wiecej info potem
 font-family: 'Montserrat', sans-serif;
 background-color: ${({theme}) => theme.secondary};
 height: 20px;
-border: none;
+border: 1px solid black;
 cursor: pointer;
 width: 100px;
 margin: 0px 10px;
-color: white;
+color: black;
 `
 export default ReactionButton
