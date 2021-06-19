ec3856fe2bb30cfbbd9515ed4dcfada27bd01a0f
Karol Szymański <karol2134@op.pl>
2021-06-19 16:22:42 +0200

    added new file: PostDetailsTEmplate.js

---
+++ src/components/templates/PostDetailsTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+
+const PostDetailTemplate = () => 
+(
+    <div>
+        <h1>Note</h1>
+        <p>
+            asdasdasdasdasdasdsadasdasdasdasdasdasdsa
+        </p>
+        <Link to="/">Powrót na główną</Link>
+    </div>
+)
+
+export default PostDetailTemplate;
