f24319094e156823f76e80459ffe0afabc273a27
Karol Szymański <karol2134@op.pl>
2021-06-19 15:23:26 +0200

    added table of objects in Posts.js

--- src/views/Posts.js
+++ src/views/Posts.js
@@ -3,6 +3,32 @@ import styled from 'styled-components';
 import PostCard from '../components/molecules/PostCard/PostCard'
 import UserPageTemplete from '../components/templates/UserPageTemplete'
 
+const posts = 
+[
+    {
+        user: 'Karol Szymański',
+        content: 'Jestem super ale React jest lepszy'
+    },
+    {
+        user: 'Karol Szymański',
+        content: 'Jestem super ale JS jest lepszy'
+    },
+    {
+        user: 'Karol Szymański',
+        content: 'Jestem super ale Node jest lepszy'
+    },
+    {
+        user: 'Karol Szymański',
+        content: 'Jestem super ale Angular jest lepszy'
+    },
+    {
+        user: 'Karol Szymański',
+        content: 'Jestem super ale MongoDB jest lepszy'
+    },
+]
+
+
+
 const Container = styled.div`
 display: flex;
 flex-direction: column;
