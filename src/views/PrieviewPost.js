70a38a0665b907a42edbae2fae32b6da5e53c776
Karol Szymański <karol2134@op.pl>
2021-06-21 17:32:42 +0200

    added and edit PrievewPost.js

---
+++ src/views/PrieviewPost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostDetailsTemplate from '../components/templates/PostDetailsTemplate';
+
+
+
+const PreviewPost = () => 
+(
+    <>
+      <PostDetailsTemplate/>
+    </>
+);
+
+export default PreviewPost;
