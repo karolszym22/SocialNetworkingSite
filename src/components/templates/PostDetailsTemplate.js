5fb535226d7225bbf52c1be401c79ea7047303fd
Karol Szymański <karol2134@op.pl>
2021-06-24 17:30:13 +0200

    edit PostDetailsTemplate.js

--- src/components/templates/PostDetailsTemplate.js
+++ src/components/templates/PostDetailsTemplate.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
+import UserPageTemplete from './UserPageTemplete';
 
 const PostDetailTemplate = () => 
 (
-    <div>
+    <UserPageTemplete>
         <h1>Note</h1>
         <p>
             asdasdasdasdasdasdsadasdasdasdasdasdasdsa
         </p>
         <Link to="/">Powrót na główną</Link>
-    </div>
+    </UserPageTemplete>
 )
 
 export default PostDetailTemplate;
