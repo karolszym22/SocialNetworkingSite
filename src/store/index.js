92cd9229c5f7ef84af147c9381aad9e3697571d2
Karol Szymański <karol2134@op.pl>
2021-08-08 21:35:13 +0200

    add proptypes for Posts.js and add window.__REDUX_DEVTOOLS_EXTENSION__ && for store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,19 @@
 import { createStore } from  'redux';
 import posts from '../reducers/index'
-const store = createStore(posts);
+
+
+
+
+
+const store = createStore(posts,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__(),);
 
 export default store;
+
+
+
+
+
+    
+
--- src/views/Posts.js
+++ src/views/Posts.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import PostCard from '../components/molecules/PostCard/PostCard'
 import UserPageTemplete from '../components/templates/UserPageTemplete'
 import { connect } from 'react-redux';
-
+import PropTypes from 'prop-types';
 
 
 const Container = styled.div`
@@ -36,7 +36,17 @@ const mapStateToProps = state =>
   const { posts } =  state
   return { posts };
 }
-    
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+       id: PropTypes.number.isRequired,
+       user: PropTypes.string.isRequired,
+       content: PropTypes.string.isRequired,
+       
+    })
+  )
+}
 
 
 export default connect(mapStateToProps) (Posts);
