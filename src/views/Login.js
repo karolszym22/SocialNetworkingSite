99bce7dc9ee80245617ed62716b6cfbb147c431c
Karol Szymański <karol2134@op.pl>
2021-10-25 19:22:54 +0200

    added axios.post in Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,13 +7,25 @@ const Login = () =>
     <div>
         <Formik
             initialValues = {{username: '', password: ''}}
-            onSubmit={()=> {axios.post('localhost:3000/api/user/login');}}
+            onSubmit={({username, password})=> {
+                axios.post('http://localhost:3000/api/user/login',
+                {
+                    username,
+                    password,
+
+                }
+                 
+
+            ).then(()=> console.log("udało się!"))
+            .catch(()=>console.log("nie udało się :///"))
+            
+            ;}}
             >
                 {()=>(
                     <Form>
                     <Field name="username" type="text"/>
                     <Field name="password" type="password"/>
-                    <button type='submit'> sign in </button>
+                    <button type='submit'>Zaloguj się</button>
                     </Form>
                 )}
         </Formik>
