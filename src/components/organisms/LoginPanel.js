6fa3ca367eb738854143fb0ee3982bc9e0979d65
Karol Szymański <karol2134@op.pl>
2021-10-26 22:39:01 +0200

    added new styled.element: ProfileContainer

--- src/components/organisms/LoginPanel.js
+++ src/components/organisms/LoginPanel.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Formik, Form, Field } from 'formik';
+const ProfileContainer = styled.div`
+
+width: 60%;
+height: 100%;
+border: 1px solid black;
+
+`
 
 
 const Container = styled.div`
 display: flex;
-flex-direction: column;
 align-items: center;
-max-width: 100% ;
-display: flex;
+width: 80% ;
+height: 550px;
+border: 1px solid black;
 justify-content: center;
 `
 
 const SignIn = () =>
 ( 
  <Container>
+     <ProfileContainer>
+         <h1>Facebook</h1>
+     </ProfileContainer>
      <Form>
         <Field name="username" type="text"/>
         <Field name="password" type="password"/>
         <button type='submit'>Zaloguj się</button>
     </Form>
+   
  </Container>
 )
 export default SignIn;    
